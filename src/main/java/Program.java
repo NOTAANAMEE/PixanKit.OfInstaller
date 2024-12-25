42256a25c81b141887b459e75ca4d62696f105ed
ry 徐 <xry070226@gmail.com>
2024-12-25 18:58:32 -0500

    New Java Project

---
+++ src/main/java/Program.java
@@ -0,0 +1,24 @@
+import optifine.Installer;
+
+import java.io.File;
+
+public class Program{
+    public static void main(String[] args){
+        if (args.length == 0){
+            System.err.println("Usage:java -cp \"optifinePath\" jar ... folderPath");
+            System.exit(1);
+        }
+        try
+        {
+            Installer.doInstall(new File(args[0]));
+        }
+        catch (Exception ex){
+            System.err.println(ex.getMessage());
+            System.err.print("At:");
+            for (var stack : ex.getStackTrace()){
+                System.err.println(stack);
+            }
+            System.exit(1);
+        }
+    }
+}
