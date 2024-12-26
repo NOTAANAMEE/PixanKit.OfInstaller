dcec00aef8590be70f6137ce98ac0c51cfd054ae
ry 徐 <xry070226@gmail.com>
2024-12-25 20:30:06 -0500

    Last Update Maybe

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -5,12 +5,13 @@ import java.io.File;
 public class Program{
     public static void main(String[] args){
         if (args.length == 0){
-            System.err.println("Usage:java -cp \"optifinePath\" jar ... folderPath");
+            System.err.println("Usage:java -cp \"optifinePath installerpath\" folderPath");
             System.exit(1);
         }
         try
         {
             Installer.doInstall(new File(args[0]));
+            System.out.println("Installation successful");
         }
         catch (Exception ex){
             System.err.println(ex.getMessage());
